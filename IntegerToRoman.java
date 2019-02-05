f0a4ebc4a90566b30d69a42514affabaea2dbee5
cdavid16 <45406535+cdavid16@users.noreply.github.com>
2019-02-04 21:02:05 -0600

    Create IntegerToRoman.java
    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
    
    Symbol       Value
    I             1
    V             5
    X             10
    L             50
    C             100
    D             500
    M             1000
    
    For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
    
    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
    
        I can be placed before V (5) and X (10) to make 4 and 9. 
        X can be placed before L (50) and C (100) to make 40 and 90. 
        C can be placed before D (500) and M (1000) to make 400 and 900.
    
    Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
    
    Example 1:
    
    Input: 3
    Output: "III"
    
    Example 2:
    
    Input: 4
    Output: "IV"
    
    Example 3:
    
    Input: 9
    Output: "IX"
    
    Example 4:
    
    Input: 58
    Output: "LVIII"
    Explanation: L = 50, V = 5, III = 3.
    
    Example 5:
    
    Input: 1994
    Output: "MCMXCIV"
    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.

---
+++ IntegerToRoman.java
@@ -0,0 +1,59 @@
+class IntegerToRoman {
+    public String intToRoman(int num) {
+        if(num<1 || num>3999)
+            throw new IllegalArgumentException("Number must be between 1 and 3999");
+        Stack<Character> keys = createMap();
+        int cont = 0, power = 3;
+        char current = ' ';
+        char fourCase = ' ';
+        String roman="";
+        while(keys.size()>0){
+            int pow = (int)(Math.pow(10, power));
+            int currentValueInt = pow * ((cont%2)==0?1:5);
+            int mult = (int)(num/pow);
+            //evaluate 4 case
+            if((mult == 4 && (cont%2)==0) || mult ==9){
+                fourCase = current;
+                if(mult==9){
+                    keys.pop();
+                    cont++;
+                }
+            }
+
+            current = keys.pop();
+
+
+            if(fourCase == ' '){
+                for(int times = 0 ; times < (int)(num/currentValueInt); times++){
+                    roman += Character.toString(current);
+                }
+                num -= (int)(currentValueInt)*(num/currentValueInt);
+            }
+            else{
+                roman += ( Character.toString(current) + Character.toString(fourCase));
+                num -= mult*pow;
+            }
+
+
+            cont++;
+            fourCase = ' ';
+
+            power -= (cont%2);
+        }
+
+
+        return roman;
+    }
+
+    private Stack createMap(){
+        Stack keys = new Stack<>();
+        keys.add('I'); //1      //73
+        keys.add('V'); //5      //86
+        keys.add('X'); //10     //88
+        keys.add('L'); //50     //76
+        keys.add('C'); //100    //67
+        keys.add('D'); //500    //68
+        keys.add('M'); //1000   //77
+        return keys;
+    }
+}
