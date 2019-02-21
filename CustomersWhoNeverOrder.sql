88b21cb6aaa7398e01985285d38b93327b37c823
cdavid16 <45406535+cdavid16@users.noreply.github.com>
2019-02-21 15:32:39 -0600

    Create CustomersWhoNeverOrder.sql
    Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
    
    Table: Customers.
    
    +----+-------+
    | Id | Name  |
    +----+-------+
    | 1  | Joe   |
    | 2  | Henry |
    | 3  | Sam   |
    | 4  | Max   |
    +----+-------+
    
    Table: Orders.
    
    +----+------------+
    | Id | CustomerId |
    +----+------------+
    | 1  | 3          |
    | 2  | 1          |
    +----+------------+
    
    Using the above tables as example, return the following:
    
    +-----------+
    | Customers |
    +-----------+
    | Henry     |
    | Max       |
    +-----------+

---
+++ CustomersWhoNeverOrder.sql
@@ -0,0 +1,15 @@
+Create table Customers (Id int, Name varchar(255));
+Create table Orders (Id int, CustomerId int);
+Truncate table Customers;
+insert into Customers (Id, Name) values ('1', 'Joe');
+insert into Customers (Id, Name) values ('2', 'Henry');
+insert into Customers (Id, Name) values ('3', 'Sam');
+insert into Customers (Id, Name) values ('4', 'Max');
+Truncate table Orders;
+insert into Orders (Id, CustomerId) values ('1', '3');
+insert into Orders (Id, CustomerId) values ('2', '1');
+
+/* Write your PL/SQL query statement below */
+SELECT Name Customers
+FROM Customers
+WHERE Id NOT IN (SELECT CustomerId FROM Orders);
