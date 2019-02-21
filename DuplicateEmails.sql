dae92b00ae53f2d97b41e573b3caba5a6c7ab41c
cdavid16 <45406535+cdavid16@users.noreply.github.com>
2019-02-21 15:31:45 -0600

    Create DuplicateEmails.sql
    Write a SQL query to find all duplicate emails in a table named Person.
    
    +----+---------+
    | Id | Email   |
    +----+---------+
    | 1  | a@b.com |
    | 2  | c@d.com |
    | 3  | a@b.com |
    +----+---------+
    
    For example, your query should return the following for the above table:
    
    +---------+
    | Email   |
    +---------+
    | a@b.com |
    +---------+
    
    Note: All emails are in lowercase.

---
+++ DuplicateEmails.sql
@@ -0,0 +1,11 @@
+Create table Person (Id int, Email varchar(255));
+Truncate table Person;
+insert into Person (Id, Email) values ('1', 'a@b.com');
+insert into Person (Id, Email) values ('2', 'c@d.com');
+insert into Person (Id, Email) values ('3', 'a@b.com');
+
+/* Write your PL/SQL query statement below */
+SELECT Email
+FROM Person
+GROUP BY Email
+Having Count(Email) > 1
