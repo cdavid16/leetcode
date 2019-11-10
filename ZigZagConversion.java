7316fe1bbcd7d7ad05585b2b07cd74c26708ac0e
cdavid16 <45406535+cdavid16@users.noreply.github.com>
2019-11-10 00:49:55 -0600

    Create ZigZagConversion.java
    The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
    
    P   A   H   N
    A P L S I I G
    Y   I   R
    And then read line by line: "PAHNAPLSIIGYIR"
    
    Write the code that will take a string and make this conversion given a number of rows:
    
    string convert(string s, int numRows);
    Example 1:
    
    Input: s = "PAYPALISHIRING", numRows = 3
    Output: "PAHNAPLSIIGYIR"
    Example 2:
    
    Input: s = "PAYPALISHIRING", numRows = 4
    Output: "PINALSIGYAHRPI"
    Explanation:
    
    P     I    N
    A   L S  I G
    Y A   H R
    P     I

---
+++ ZigZagConversion.java
@@ -0,0 +1,45 @@
+public String convert(String s, int numRows) {
+        
+        if(numRows<=1 || numRows > s.length())
+            return s;
+        
+		int maxCols = Math.round(s.length()/2) + 1;
+		char[][] nS = new char[numRows][maxCols];
+		String ns = "";
+		int x=0, y=-1;
+		boolean addCol = false;
+		boolean addRow = false;
+		for(int i = 0; i<s.length(); i++) {
+			if(y<numRows-1) {				
+				if(y<=0) {
+					addRow = true;
+					addCol = !addRow;
+				}
+				
+				if(addRow) {
+					y++;
+				} 
+			}else {
+				if(addRow) {
+					addCol = true;
+					addRow = !addCol;						
+				}
+			}		
+			
+			if(addCol) {
+				x++;
+				y--;
+			}
+			nS[y][x] = s.charAt(i);
+		}		
+		
+		for(int i=0; i<nS.length; i++) {
+			for(int j=0;j<nS[i].length; j++) {
+				if(nS[i][j] != 0) {
+					ns += String.valueOf(nS[i][j]); 
+				}
+			}
+		}		
+		
+		return ns;
+	}
