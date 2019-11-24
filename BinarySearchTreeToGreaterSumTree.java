9efae1504effda34e4a35aea3ca85da5a1554cc7
cdavid16 <45406535+cdavid16@users.noreply.github.com>
2019-11-24 17:09:21 -0600

    Create BinarySearchTreeToGreaterSumTree.java
    Given the root of a binary search tree with distinct values, modify it so that every node has a new value equal to the sum of the values of the original tree that are greater than or equal to node.val.
    
    As a reminder, a binary search tree is a tree that satisfies these constraints:
    
    The left subtree of a node contains only nodes with keys less than the node's key.
    The right subtree of a node contains only nodes with keys greater than the node's key.
    Both the left and right subtrees must also be binary search trees.
     
    
    Example 1:
    
    
    
    Input: [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
    Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
     
    
    Note:
    
    The number of nodes in the tree is between 1 and 100.
    Each node will have value between 0 and 100.
    The given tree is a binary search tree.

---
+++ BinarySearchTreeToGreaterSumTree.java
@@ -0,0 +1,74 @@
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class BinarySearchTreeToGreaterSumTree {
+  public static class TreeNode {
+    int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+      this.val = x;
+    }
+
+    @Override
+    public String toString() {
+      return "TreeNode [val=" + this.val + ", left=" + this.left + ", right=" + this.right + "]";
+    }
+
+
+  }
+
+
+  public TreeNode bstToGst(TreeNode root) {
+    TreeMap<Integer, Integer> ocurrences = new TreeMap<Integer, Integer>();
+    Integer sum = 0;
+    TreeNode current;
+    Queue<TreeNode> bfs = new LinkedList<TreeNode>();
+    bfs.add(root);
+    while (bfs.size() > 0) {
+      current = bfs.poll();
+
+      if (current == null) {
+        continue;
+      }
+
+      sum += current.val;
+
+      if (!ocurrences.containsKey(current.val))
+        ocurrences.put(current.val, 0);
+
+      if (current.left != null)
+        bfs.add(current.left);
+      if (current.right != null)
+        bfs.add(current.right);
+    }
+
+    // scan and update how should be deducted for each key
+    Integer carry = 0;
+    for (Map.Entry<Integer, Integer> e : ocurrences.entrySet()) {
+      e.setValue(sum - carry);
+      carry += e.getKey();
+    }
+
+
+
+    bfs.add(root);
+    while (bfs.size() > 0) {
+      current = bfs.poll();
+      if (current == null) {
+        continue;
+      }
+      current.val = ocurrences.get(current.val);
+
+      if (current.left != null)
+        bfs.add(current.left);
+      if (current.right != null)
+        bfs.add(current.right);
+    }
+
+    return root;
+  }
+}
