68c68a3a44a375f36e7c09686c48c085f6a07ccb
c_david <c_david@192.168.0.135>
2020-04-04 13:01:58 -0600

    Adding LC Problems: 1295. Find Numbers with Even Number of Digits 1389. Create Target Array in the Given Order 1281. Subtract the Product and Sum of Digits of an Integer

---
+++ leetcode/algorithms/TagetArray.java
@@ -0,0 +1,53 @@
+package leetcode.algorithms;
+
+/*
+ * Given two arrays of integers nums and index. Your task is to create target array under the
+ * following rules:
+ * 
+ * Initially target array is empty. From left to right read nums[i] and index[i], insert at index
+ * index[i] the value nums[i] in target array. Repeat the previous step until there are no elements
+ * to read in nums and index. Return the target array.
+ * 
+ * It is guaranteed that the insertion operations will be valid.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [0,1,2,3,4], index = [0,1,2,2,1] Output: [0,4,1,3,2] Explanation: nums index target
+ * 0 0 [0] 1 1 [0,1] 2 2 [0,1,2] 3 2 [0,1,3,2] 4 1 [0,4,1,3,2] Example 2:
+ * 
+ * Input: nums = [1,2,3,4,0], index = [0,1,2,3,0] Output: [0,1,2,3,4] Explanation: nums index target
+ * 1 0 [1] 2 1 [1,2] 3 2 [1,2,3] 4 3 [1,2,3,4] 0 0 [0,1,2,3,4] Example 3:
+ * 
+ * Input: nums = [1], index = [0] Output: [1]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= nums.length, index.length <= 100 nums.length == index.length 0 <= nums[i] <= 100 0 <=
+ * index[i] <= i
+ */
+public class TagetArray {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[index.length];
+        //initialize all values to -1
+        for(int i=0;i<target.length;i++)
+            target[i]=-1;
+        
+        for(int i=0;i<index.length;i++){
+            if(target[index[i]]==-1){ //check if space is available
+                target[index[i]] = nums[i];
+            }
+            else {
+                int j; 
+                for(j=index.length-1; j>index[i];j--){  //reorder the array                
+                    target[j] = target[j-1]; 
+                }
+                target[index[i]] = nums[i]; 
+            }
+        }
+        
+        return target;
+    }
+}
