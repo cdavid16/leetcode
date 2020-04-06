e2b838f90de942ce091bd34eb66b7a5178402ec8
c_david <c_david@localhost>
2020-04-05 22:28:30 -0500

    LC 938. Range Sum of BST

---
+++ leetcode/algorithms/RangeSumBst.java
@@ -0,0 +1,53 @@
+package leetcode.algorithms;
+
+import java.util.Stack;
+
+/*
+ * Given the root node of a binary search tree, return the sum of values of all nodes with value
+ * between L and R (inclusive).
+ * 
+ * The binary search tree is guaranteed to have unique values.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: root = [10,5,15,3,7,null,18], L = 7, R = 15 Output: 32 Example 2:
+ * 
+ * Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10 Output: 23
+ * 
+ * 
+ * Note:
+ * 
+ * The number of nodes in the tree is at most 10000. The final answer is guaranteed to be less than
+ * 2^31.
+ */
+public class RangeSumBst {
+
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        int sum=0;
+        Stack<TreeNode> bfs = new Stack<>();
+        bfs.add(root);
+        while(bfs.size()>0){
+            TreeNode tmp = bfs.pop();            
+            sum+=(tmp.val >= L && tmp.val<=R)?tmp.val:0;
+            if(tmp.left!=null)
+                bfs.add(tmp.left);
+            if(tmp.right!=null)
+                bfs.add(tmp.right);
+        }
+        return sum;
+    }
+    
+}
+
+
+  //Definition for a binary tree node.
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+}
+ 
+
